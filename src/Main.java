9788d9c42a3203ee974913563281261933a2a3e5
Archimedes314 <63058147+Archimedes314@users.noreply.github.com>
2020-11-12 15:31:34 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws IllegalDirectionException, InterruptedException {
 
 
-        GameView game= new GameView(20,20,9);
+        GameView game= new GameView(20,20,9);   //initiialize a 20 by 20 block grid with snake length of 9 blocks
 
 
 
